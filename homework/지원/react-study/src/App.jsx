af2763b93b48ddfb61234d92981357df5440a388
mz_one <limjiwon1113@gmail.com>
2024-04-23 22:06:21 +0900

    숙제중

---
+++ homework/지원/react-study/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ToggleSwitch from './ToggleSwitch';
+import Practice02 from './Gallery'
+
+function App() {
+  return (
+    <div>
+      <ToggleSwitch></ToggleSwitch>
+      <Practice02></Practice02>
+    </div>
+  );
+}
+
+export default App;
