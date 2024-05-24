615b970b2527dc7a2d40fad8ff58ed3c02a45984
CHOGYEONGMOON <chonghocho72@gmail.com>
2024-05-24 17:58:43 +0900

    코딩리스트 로직 분리 및 리팩토링 중

--- homework/cpt_2/src/main.jsx
+++ homework/cpt_2/src/main.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './App.jsx'
-import Todo from './views/유람/component/Todo'
-import UserStatus from './views/유람/component/UserStatus'
+import App from './App.jsx'
+// import Todo from './views/유람/component/Todo'
+// import UserStatus from './views/유람/component/UserStatus'
 import './index.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Todo />
-    <UserStatus/>
+    <App />
+    {/* <Todo />
+    <UserStatus /> */}
   </React.StrictMode>,
 )
